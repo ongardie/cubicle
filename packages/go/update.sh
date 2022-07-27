350d61f7d92b170e84ffbb5978ee6dcc56f0dae6
Diego Ongaro <ongardie@gmail.com>
2022-07-27 13:06:02 -0700

    packages/go: Don't assume there's a system Go installation

--- packages/go/update.sh
+++ packages/go/update.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
-set -ex
+set -eu
 
-# Debian's bullseye-backports Go version seems recent enough.
-go version
+CACHE_HOME=${XDG_CACHE_HOME:-"$HOME/.cache"}
+mkdir -p "$CACHE_HOME"
+
+echo "Checking latest version of Go"
+RELEASES="$CACHE_HOME/go-releases.json"
+curl -s 'https://go.dev/dl/?mode=json' > "$RELEASES"
+version=$(jq -r 'map(select(.stable) | .version)[0]' < "$RELEASES")
+
+installed=$(go env GOVERSION || true)
+echo "Have $installed"
+
+if [ x"$installed" != x"$version" ]; then
+    if [ ! -f "$version.linux-amd64.tar.gz" ]; then
+        echo "Downloading $version"
+        curl -LO "https://go.dev/dl/$version.linux-amd64.tar.gz"
+    fi
+
+    echo "Unpacking $version"
+    mkdir -p ~/opt
+    rm -rf ~/opt/go
+    pv "$version.linux-amd64.tar.gz" | tar -xz -C ~/opt
+fi
+
+mkdir -p ~/bin
+ln -fs ~/opt/go/bin/go ~/bin/
+
+if [ x"$(go env GOVERSION)" != x"$version" ]; then
+    echo "ERROR: Have $(go env GOVERSION), which is still not right"
+    exit 1
+fi
+
+set -x
 
 # Tools used by the Go VS Code extension.
 go install github.com/go-delve/delve/cmd/dlv@latest
