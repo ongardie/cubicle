1024607012b5ef64b2b9b977b201926f15b1416f
Diego Ongaro <ongardie@gmail.com>
2022-08-09 12:25:41 -0700

    rust: Start CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Parser, Subcommand};
+
+/// Manage sandboxed development environments.
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Run a shell in an existing environment.
+    Enter {
+        /// Environment name.
+        name: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    todo!("{:#?}", args);
 }
