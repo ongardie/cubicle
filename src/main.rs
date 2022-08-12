58573a66ce0b10ff124cf57ceece4a8018fa1111
Diego Ongaro <ongardie@gmail.com>
2022-08-11 23:39:14 -0700

    rust: Implement Bubblewrap runner
    The Rust version is now at feature parity with the Python version.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use rand::seq::SliceRandom;
 use regex::{Regex, RegexBuilder};
 use serde::Serialize;
 use std::collections::{BTreeMap, BTreeSet};
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
 use std::fmt;
 use std::io::{self, BufRead, Write};
 use std::iter;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 use std::path::{Path, PathBuf};
-use std::process::{Command, ExitStatus, Stdio};
+use std::process::{Child, ChildStdout, Command, ExitStatus, Stdio};
 use std::str::FromStr;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
@@ -183,12 +183,12 @@ impl Cubicle {
             let mut depends = read_package_list(&dir, "depends.txt")?.unwrap_or_default();
             depends.insert(PackageName::from_str("auto").unwrap());
             let test = if dir.join("test.sh").exists() {
-                Some(String::from("test.sh"))
+                Some(String::from("./test.sh"))
             } else {
                 None
             };
             let update = if dir.join("update.sh").exists() {
-                Some(String::from("update.sh"))
+                Some(String::from("./update.sh"))
             } else {
                 None
             };
@@ -1212,11 +1212,23 @@ impl Cubicle {
         F: Fn(&dyn Runner) -> Result<T>,
     {
         match self.runner {
-            RunnerKind::Bubblewrap => todo!("bubblewrap runner"),
+            #[cfg(target_os = "linux")]
+            RunnerKind::Bubblewrap => func(&Bubblewrap { program: self }),
             RunnerKind::Docker => func(&Docker { program: self }),
         }
     }
+}
 
+#[cfg(target_os = "linux")]
+fn ro_bind_try<P: AsRef<Path>>(path: P) -> [OsString; 3] {
+    return [
+        OsString::from("--ro-bind-try"),
+        path.as_ref().as_os_str().to_owned(),
+        path.as_ref().as_os_str().to_owned(),
+    ];
+}
+
+impl Cubicle {
     fn packages_to_seeds(&self, packages: &PackageNameSet) -> Result<Vec<PathBuf>> {
         let mut seeds = Vec::with_capacity(packages.len());
         let specs = self.scan_packages()?;
@@ -1314,6 +1326,188 @@ struct RunnerRunArgs<'a> {
     host_work: &'a Path,
 }
 
+#[cfg(target_os = "linux")]
+struct Bubblewrap<'a> {
+    program: &'a Cubicle,
+}
+
+struct ScopedChild(Child);
+
+impl Deref for ScopedChild {
+    type Target = Child;
+    fn deref(&self) -> &Child {
+        &self.0
+    }
+}
+
+impl DerefMut for ScopedChild {
+    fn deref_mut(&mut self) -> &mut Child {
+        &mut self.0
+    }
+}
+
+impl Drop for ScopedChild {
+    fn drop(&mut self) {
+        if let Ok(None) = self.0.try_wait() {
+            let _ = self.0.kill();
+            let _ = self.0.wait();
+        }
+    }
+}
+
+trait ScopedSpawn {
+    fn scoped_spawn(&mut self) -> std::io::Result<ScopedChild>;
+}
+
+impl ScopedSpawn for Command {
+    fn scoped_spawn(&mut self) -> std::io::Result<ScopedChild> {
+        Ok(ScopedChild(self.spawn()?))
+    }
+}
+
+#[cfg(target_os = "linux")]
+fn get_fd_for_child<F>(file: &F) -> Result<String>
+where
+    F: rustix::fd::AsFd + std::os::unix::io::AsRawFd,
+{
+    // This is pretty ugly, but it's how bwrap likes it.
+    let mut flags = rustix::fs::fcntl_getfd(file)?;
+    flags.remove(rustix::fs::FdFlags::CLOEXEC);
+    rustix::fs::fcntl_setfd(&file, flags)?;
+    Ok(file.as_raw_fd().to_string())
+}
+
+#[cfg(target_os = "linux")]
+impl<'a> Runner for Bubblewrap<'a> {
+    fn kill(&self, _name: &EnvironmentName) -> Result<()> {
+        // nothing to do
+        Ok(())
+    }
+
+    fn run(
+        &self,
+        name: &EnvironmentName,
+        RunnerRunArgs {
+            command: run_command,
+            host_home,
+            host_work,
+        }: &RunnerRunArgs,
+    ) -> Result<()> {
+        struct Seed {
+            _child: ScopedChild, // this is here so its destructor will reap it later
+            stdout: ChildStdout,
+        }
+        let seed = if let RunnerCommand::Init { seeds, .. } = run_command {
+            println!("Packing seed tarball");
+            let mut child = Command::new("pv")
+                .args(["-i", "0.1"])
+                .stdout(Stdio::piped())
+                .args(seeds)
+                .scoped_spawn()?;
+            let stdout = child.stdout.take().unwrap();
+            Some(Seed {
+                _child: child,
+                stdout,
+            })
+        } else {
+            None
+        };
+
+        let seccomp = std::fs::File::open(self.program.script_path.join("seccomp.bpf"))?;
+
+        let mut command = Command::new("bwrap");
+
+        command.env_clear();
+        command.env(
+            "PATH",
+            match self.program.home.to_str() {
+                Some(home) => format!("{home}/bin:/bin:/sbin"),
+                None => String::from("/bin:/sbin"),
+            },
+        );
+        command.env("SANDBOX", name);
+        command.env("TMPDIR", self.program.home.join("tmp"));
+        for key in ["DISPLAY", "HOME", "SHELL", "TERM"] {
+            if let Ok(value) = std::env::var(key) {
+                command.env(key, value);
+            }
+        }
+
+        command.arg("--die-with-parent");
+        command.arg("--unshare-cgroup");
+        command.arg("--unshare-ipc");
+        command.arg("--unshare-pid");
+        command.arg("--unshare-uts");
+
+        command.arg("--hostname");
+        match &self.program.hostname {
+            Some(hostname) => command.arg(format!("{name}.{hostname}")),
+            None => command.arg(name),
+        };
+
+        command.args(["--symlink", "/usr/bin", "/bin"]);
+        command.args(["--dev", "/dev"]);
+
+        if let RunnerCommand::Init { script, .. } = run_command {
+            command
+                .arg("--ro-bind-try")
+                .arg(script)
+                .arg("/cubicle-init.sh");
+        }
+
+        if let Some(Seed { stdout, .. }) = &seed {
+            command
+                .arg("--file")
+                .arg(get_fd_for_child(stdout)?)
+                .arg("/dev/shm/seed.tar");
+        }
+        command.args(ro_bind_try("/etc"));
+        command.arg("--bind").arg(host_home).arg(&self.program.home);
+        command
+            .arg("--bind")
+            .arg(host_work)
+            .arg(self.program.home.join(name));
+        command.args(["--symlink", "/usr/lib", "/lib"]);
+        command.args(["--symlink", "/usr/lib64", "/lib64"]);
+        command.args(ro_bind_try("/opt"));
+        command.args(["--proc", "/proc"]);
+        command.args(["--symlink", "/usr/sbin", "/sbin"]);
+        command.args(["--tmpfs", "/tmp"]);
+        command.args(ro_bind_try("/usr"));
+        command.args(ro_bind_try("/var/lib/apt/lists"));
+        command.args(ro_bind_try("/var/lib/dpkg"));
+        command.arg("--seccomp").arg(get_fd_for_child(&seccomp)?);
+        command.arg("--chdir").arg(self.program.home.join(name));
+        command.arg("--");
+        command.arg(&self.program.shell);
+        command.arg("-l");
+
+        match run_command {
+            RunnerCommand::Interactive => {}
+            RunnerCommand::Init { .. } => {
+                command.args(["-c", "/cubicle-init.sh"]);
+            }
+            RunnerCommand::Exec(exec) => {
+                command.arg("-c");
+                // `shlex.join` doesn't work directly since `exec` has
+                // `String`s, not `str`s.
+                command.arg(
+                    exec.iter()
+                        .map(|a| shlex::quote(a))
+                        .collect::<Vec<_>>()
+                        .join(" "),
+                );
+            }
+        }
+
+        let status = command.status()?;
+        if !status.success() {
+            Err(ExitStatusError::new(status))?;
+        }
+        Ok(())
+    }
+}
+
 struct Docker<'a> {
     program: &'a Cubicle,
 }
@@ -1368,7 +1562,7 @@ impl<'a> Docker<'a> {
         let mut child = Command::new("docker")
             .args(["build", "--tag", "cubicle-base", "-"])
             .stdin(Stdio::piped())
-            .spawn()?;
+            .scoped_spawn()?;
         {
             let mut stdin = child
                 .stdin
@@ -1480,19 +1674,27 @@ impl<'a> Runner for Docker<'a> {
         Ok(())
     }
 
-    fn run(&self, name: &EnvironmentName, args: &RunnerRunArgs) -> Result<()> {
+    fn run(
+        &self,
+        name: &EnvironmentName,
+        RunnerRunArgs {
+            command: run_command,
+            host_home,
+            host_work,
+        }: &RunnerRunArgs,
+    ) -> Result<()> {
         if !self.is_running(name)? {
             self.build_base()?;
             self.spawn(
                 name,
                 &DockerSpawnArgs {
-                    host_home: args.host_home,
-                    host_work: args.host_work,
+                    host_home,
+                    host_work,
                 },
             )?;
         }
 
-        if let RunnerCommand::Init { script, seeds } = args.command {
+        if let RunnerCommand::Init { script, seeds } = run_command {
             // TODO: this could probably write the script directly into
             // docker exec's stdin instead.
             let status = Command::new("docker")
@@ -1544,7 +1746,7 @@ impl<'a> Runner for Docker<'a> {
                         ),
                     ])
                     .stdin(Stdio::piped())
-                    .spawn()?;
+                    .scoped_spawn()?;
                 {
                     let mut stdin = child
                         .stdin
@@ -1589,7 +1791,7 @@ impl<'a> Runner for Docker<'a> {
         command.arg("--tty");
         command.arg(name);
         command.args([&self.program.shell, "-l"]);
-        match args.command {
+        match run_command {
             RunnerCommand::Interactive => {}
             RunnerCommand::Init { .. } => {
                 command.args(["-c", "/cubicle-init.sh"]);
@@ -1746,8 +1948,8 @@ impl std::convert::AsRef<Path> for EnvironmentName {
     }
 }
 
-impl std::convert::AsRef<std::ffi::OsStr> for EnvironmentName {
-    fn as_ref(&self) -> &std::ffi::OsStr {
+impl std::convert::AsRef<OsStr> for EnvironmentName {
+    fn as_ref(&self) -> &OsStr {
         self.0.as_ref()
     }
 }
@@ -1819,18 +2021,22 @@ enum ListPackagesFormat {
 }
 
 enum RunnerKind {
+    #[cfg(target_os = "linux")]
     Bubblewrap,
     Docker,
 }
 
 fn get_runner(script_path: &Path) -> Result<RunnerKind> {
     let runner_path = script_path.join(".RUNNER");
-    let runners = "'bubblewrap' or 'docker'";
+    let runners = "'bubblewrap' (on Linux only) or 'docker'";
     match std::fs::read_to_string(&runner_path)
         .with_context(|| format!("Could not read {:?}. Expected {}.", runner_path, runners))?
         .trim()
     {
+        #[cfg(target_os = "linux")]
         "bubblewrap" => Ok(RunnerKind::Bubblewrap),
+        #[cfg(not(target_os = "linux"))]
+        "bubblewrap" => Err(anyhow!("Bubblewrap is only supported on Linux")),
         "docker" => Ok(RunnerKind::Docker),
         r => Err(anyhow!(
             "Unknown runner in {:?}: {:?}. Expected {}.",
