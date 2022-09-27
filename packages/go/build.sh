33abb6b42c411ca01f9aeaa46a1ba062452fa55d
Diego Ongaro <ongardie@gmail.com>
2022-09-26 21:07:46 -0700

    packages/go: Switch to asdf

--- packages/go/build.sh
+++ packages/go/build.sh
@@ -1,50 +1,17 @@
 #!/bin/sh
 set -eu
-cd
 
+asdf plugin list | grep '^golang$' || asdf plugin add golang
 
-echo 'export GOROOT=$HOME/opt/go' > ~/.profile.d/70-goroot.sh
-echo '$HOME/go/bin/' > ~/.profile.d/path/32-go
+asdf install golang latest
 
-CACHE_HOME=${XDG_CACHE_HOME:-"$HOME/.cache"}
-mkdir -p "$CACHE_HOME"
+mkdir -p ~/.dev-init/
+echo 'asdf global golang latest' > ~/.dev-init/go-asdf.sh
+chmod +x ~/.dev-init/go-asdf.sh
 
-echo "Checking latest version of Go"
-RELEASES="$CACHE_HOME/go-releases.json"
-curl -s 'https://go.dev/dl/?mode=json' > "$RELEASES"
-version=$(jq -r 'map(select(.stable) | .version)[0]' < "$RELEASES")
-
-installed=$(go env GOVERSION || true)
-installed_root=$(go env GOROOT || true)
-echo "Have $installed installed at $installed_root"
-
-if [ "$installed" != "$version" ] || [ "$installed_root" != "$HOME/opt/go" ]; then
-    if [ ! -f "$version.linux-amd64.tar.gz" ]; then
-        echo "Downloading $version"
-        curl -LO "https://go.dev/dl/$version.linux-amd64.tar.gz"
-    fi
-
-    echo "Unpacking $version"
-    mkdir -p ~/opt
-    rm -rf ~/opt/go
-    pv "$version.linux-amd64.tar.gz" | tar -xz -C ~/opt
-fi
-
-mkdir -p ~/bin
-ln -fs ../opt/go/bin/go ~/bin/
-
-installed=$(go env GOVERSION || true)
-installed_root=$(go env GOROOT || true)
-if [ "$installed" != "$version" ] || [ "$installed_root" != "$HOME/opt/go" ]; then
-    echo "ERROR: Have $installed installed at $installed_root, which is still not right"
-    exit 1
-fi
-
-set -x
-
-# Tools used by the Go VS Code extension.
-go install github.com/go-delve/delve/cmd/dlv@latest
-go install golang.org/x/tools/gopls@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
+asdf global golang latest
+install="$(asdf where golang)/packages/bin"
+install="$HOME${install#$HOME}"
+echo "$install" > ~/.configs/profile.d/path/36-go
 
 tar -c -C ~ --verbatim-files-from --files-from ~/w/provides.txt -f ~/provides.tar
