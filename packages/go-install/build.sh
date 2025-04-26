3d25a3072f337a83fccddbe3df0a531a6f966be9
Diego Ongaro <ongardie@gmail.com>
2025-04-25 20:41:04 -0700

    Update go package following asdf-golang path change

--- packages/go-install/build.sh
+++ packages/go-install/build.sh
@@ -5,7 +5,7 @@ cd
 bin=$(basename "$PACKAGE")
 go install "$PACKAGE"@latest
 asdf reshim golang
-install="$(asdf where golang)/packages/bin/$bin"
+install="$(asdf where golang)/bin/$bin"
 install="${install#"$HOME/"}"
 tar --create --verbose --file provides.tar \
     "$install" \
