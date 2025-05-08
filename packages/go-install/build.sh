5854235e609bdec2b317c588ce77acf34491bf74
Diego Ongaro <ongardie@gmail.com>
2025-05-07 18:43:12 -0700

    Port go-install build script to Nushell

--- packages/go-install/build.sh
+++ packages/go-install/build.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
-set -eu
 
-cd
-bin=$(basename "$PACKAGE")
-go install "$PACKAGE"@latest
-asdf reshim golang
-install="$(asdf where golang)/bin/$bin"
-install="${install#"$HOME/"}"
-tar --create --verbose --file provides.tar \
-    "$install" \
-    ".asdf/shims/$bin"
+exec ./build.nu
